8e94cb87289bbc40d09231fb3b5e48725e2b68aa
Matt Royer <matthew.royer@gmail.com>
2014-01-28 09:43:16 -0700

    Created lib folder and moved files. Also renamed app.rb to plume.rb. And added comments to plume.rb

--- lib/plume.rb
+++ lib/plume.rb
@@ -2,8 +2,10 @@ require 'yaml'
 require 'erb'
 %w(file_tools registry).each { |lib| require_relative lib }
 
-settings = YAML::load_file("settings.yml")
+# Load example YAML settings file
+settings = YAML::load_file("../example/settings.yml")
 
+# Create all folders from settings.yml
 settings['folders'].each do |folder|
   current_folder = ERB.new(folder[1]['path']).result
   subs = folder[1]['sub_folders']
@@ -13,11 +15,14 @@ settings['folders'].each do |folder|
   end
 end
 
+# Create all files from settings.yml
 settings['files'].each do |file|
   current_file = FileTools.new ERB.new(file[1]['path']).result, file[1]['filename'], file[1]['content']
   current_file.create_file
 end
 
+# Create/edit all entries from settings.yml
+# Warning: No error checking yet. Registry will change
 settings['registry'].each do |registry|
   reg = Registry.new registry[1]['base'], registry[1]['location'], registry[1]['key'], registry[1]['value']
   reg.set_registry
